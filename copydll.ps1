f8875fba245dcbdc114098a5cd92327a8ccc2d3d
segfo <k.segfo@gmail.com>
2023-01-28 17:29:53 +0900

    :bug: ps1スクリプトをトラック対象外にした。

--- copydll.ps1
+++
@@ -1,6 +0,0 @@
-$dest_dirs=@("D:\Users\segfo\Downloads\plugins","D:\Users\segfo\Downloads\full_package\multiline_paster_plugins","$Env:userprofile\.cargo\bin\multiline_paster_plugins")
-$src=".\target\debug\main_logic.dll"
-
-foreach ($dest in $dest_dirs){
-    copy -Force "$src" "$dest"
-}
